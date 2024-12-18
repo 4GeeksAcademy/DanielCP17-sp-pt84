0da6a1ffdce0d4889bea5419b200066ded8f4fa5
Daniel Campón Perdigones <campondaniel@gmail.com>
2024-12-18 19:33:51 +0000

    Creada Estructura de datos y API para interactuar ✓ The Family structure has to be initialized with the 3 members specified in the instructions ✓ Implement method POST /member to add a new member ✓ Method POST /member should return something, NOT EMPTY ✓ Implement method GET /members ✓ Method GET /members should return a list ✓ We added two members using POST /member, when calling the GET /members should get a list of length == 5 ✓ Method GET /member/<int:id> should exist ✓ Method GET /member/<int:id> should return a one single family member in a dictionary format ✓ The dictionary returned by GET /member/<int:id> should contain one family member with the keys [name, id, age, lucky_numbers] ✓ Method GET /member/3443 should return Tommy ✓ Implement method DELETE /member/<int:id> to delete a family member ✓ Method DELETE /member/3443 should return dictionary with 'done' key ✓ After deleting the member 3443 we called GET /members and it should return a list with 4 members

--- src/app.py
+++ src/app.py
@@ -2,8 +2,8 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for
-from flask_cors import CORS
+from flask import Flask, request, jsonify, url_for # type: ignore
+from flask_cors import CORS # type: ignore
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
 #from models import Person
@@ -30,13 +30,50 @@ def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
+    response_body = members
 
     return jsonify(response_body), 200
+
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member(id):
+    member = jackson_family.get_member(id)
+    if member:
+        return jsonify(member), 200
+    return jsonify({'error': 'Member not found'}), 404
+    
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    body = request.get_json(silent=True)
+    if body is None:
+        return jsonify({'msg': 'Debes enviar información en el body'}), 400
+    if 'id' not in body:
+        body['id'] = jackson_family._generateId()
+    if 'first_name' not in body:
+        return jsonify({'msg': 'El campo first_name es obligatorio'}), 400
+    if 'age' not in body:
+        return jsonify({'msg': 'El campo age es obligatorio'}), 400
+    if 'lucky_numbers' not in body:
+        return jsonify({'msg': 'El campo lucky_numbers es obligatorio'}), 400
+
+    new_member = {
+        'id': body['id'],
+        'first_name': body['first_name'],
+        'last_name': jackson_family.last_name,
+        'age': body['age'],
+        'lucky_numbers': body['lucky_numbers']
+    }
+
+    jackson_family.add_member(new_member)
+    return jsonify({'msg': 'ok','new member': new_member}), 200
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    delete_member = jackson_family.delete_member(id)
+    if delete_member:
+        return jsonify(delete_member), 200
+    return jsonify({'error': 'Member not found'}), 404
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
